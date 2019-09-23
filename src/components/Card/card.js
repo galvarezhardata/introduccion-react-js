87c96f5dbed13c9fcb600320c48c3724f42faa62
Germán Rodrigo Alvarez <g.alvarez@hardata.com>
2019-09-22 23:45:06 -0300

    Implementa el componente Card y corrige unos warnings del App.js

--- src/components/Card/card.js
+++ src/components/Card/card.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import '../Card/card.css'
 
 export default class Card extends Component {
     static propTypes = {
@@ -17,10 +18,8 @@ export default class Card extends Component {
         const { image, text, title } = this.props
 
         return (
-            <div 
-                className="card" 
-                style={{ width: "18rem" }}>
-                <img className="card-img-top" src={image} alt="" />
+            <div className="card app-card">
+                <img className="card-img-top" src={image} alt={`${title}`} />
                 <div className="card-body">
                     <h5 className="card-title">{title}</h5>
                     <p className="card-text">{text}</p>
