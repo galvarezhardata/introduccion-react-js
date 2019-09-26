b4b21ea8ebb5c8752c9b0807ee69a405c1216731
Germán Rodrigo Alvarez <g.alvarez@hardata.com>
2019-09-26 17:55:36 -0300

    Instala y configura react-router.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,62 @@
 import React from 'react';
+import { Switch, Route } from "react-router-dom"
 import './App.css';
 
 import Header from './components/Header/header'
-import Slideshow from './components/Slideshow/slideshow'
 import Footer from './components/Footer/footer'
+import Slideshow from './components/Slideshow/slideshow'
+import Info from './components/Info/info'
+import NotFound from './components/NotFound/notFound'
 
 function App() {
   return (
     <div>
-
-      <div className="container-fluid">
-        <div className="row">
-            <Header />
-        </div>
-      </div>
-
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col">
-            <Slideshow />
-          </div>
-        </div>
-      </div>
-
-      {/* <div className="container-fluid">
-        <div className="row">
-          <Slideshow />
-        </div>
-      </div> */}
-        
-      <div className="container-fluid">
-        <div className="row">
-            <Footer />
-        </div>
-      </div>
-
+      <Header />
+      <Switch>
+        <Route exact path="/home" component={Slideshow} />
+        <Route exact path="/" component={Slideshow} />
+        <Route component={NotFound} />
+      </Switch>
+      <Footer />
     </div>
+    // <div>
+
+    //   <div className="container-fluid">
+    //     <div className="row">
+    //         <Header />
+    //     </div>
+    //   </div>
+
+    //   <div className="container-fluid">
+    //     <div className="row">
+
+    //       <div className="col-md-auto">
+    //         <Slideshow />
+    //       </div>
+
+    //       <div className="col pt-1">
+    //         <Info
+    //           imageDefMale='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
+    //           imageShiMale='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
+    //           imageDefFemale='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
+    //           imageShiFemale='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
+    //           title='Bulbasaur '
+    //           type1='grass'
+    //           type2='grass'
+    //           subtitle="Lorem ipsum dolor sit amet..."
+    //           text="Consectetur adipiscing elit. Etiam a nibh eu enim ornare egestas sit amet id orci. Cras varius mauris vitae dolor sollicitudin gravida. In ut faucibus massa. Mauris mattis tempor nulla posuere eleifend. Duis rhoncus rutrum nibh, at vulputate sapien tincidunt sit amet. Nulla quis purus eros. Proin convallis vestibulum risus. Integer lobortis, lacus eget suscipit congue, magna diam elementum nulla, et congue mauris tortor vel tortor." />
+    //       </div>
+        
+    //     </div>
+    //   </div>
+
+    //   <div className="container-fluid">
+    //     <div className="row">
+    //         <Footer />
+    //     </div>
+    //   </div>
+
+    // </div>
   );
 }
 
